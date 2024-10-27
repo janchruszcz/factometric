5be30ff759cce331011ac7600790bb00d3c48cfe
Jan Chruszcz <jrchruszcz@gmail.com>
2024-10-27 15:04:26 +0100

    Setup minitest, cors, pagy

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  # skip_before_action :verify_authenticity_token
+  before_action :set_cors_headers
+
+  private
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+  end
 end
